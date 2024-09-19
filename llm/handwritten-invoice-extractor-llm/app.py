4516301dce7f692ede1ddd74afe115bee15dee9d
jytbard <jytbard@gmail.com>
2024-09-19 22:08:04 +0530

    1st

---
+++ llm/handwritten-invoice-extractor-llm/app.py
@@ -0,0 +1,38 @@
+import streamlit as st
+from helpers import *
+
+
+def main():
+    st.set_page_config(page_title="Invoice Extraction to CSV")
+    
+    # Upload Bills
+    pdf_files = st.file_uploader("Upload invoice in PDF format only",
+                                 type=["pdf"],
+                                 accept_multiple_files=True)
+    extract_button = st.button("Extract bill data...")
+    
+    if extract_button:
+        with st.spinner("Extracting PDF file data..."):
+            data_frame = create_docs(pdf_files)
+            st.write(data_frame.head())
+            data_frame["AMOUNT"] = data_frame["AMOUNT"].astype(float)
+            st.write("Average bill amount: ", data_frame['AMOUNT'].mean())
+            
+            # convert to csv
+            convert_to_csv = data_frame.to_csv(index=False).encode("utf-8")
+            
+            
+            st.download_button(
+                "Download data as CSV",
+                convert_to_csv,
+                "CSV_Bills.csv",
+                "text/csv",
+                key="download-csv"
+            )
+        st.success("Success!!")
+            
+    
+
+#Invoking main function
+if __name__ == '__main__':
+    main()
